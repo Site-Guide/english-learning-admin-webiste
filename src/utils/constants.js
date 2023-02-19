f945c7669182834d23013d11643065dcd4e94e74
Tushar Deepak <tushardeepak22@gmail.com>
2023-02-19 10:51:57 +0530

    production mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 const PROJECT_ID = "63ca393fc7b7f28ab286";
-const ENDPOINT = "http://appwrite.engexpert.in/v1";
+const ENDPOINT =
+  process.env.NODE_ENV == "development"
+    ? "http://appwrite.engexpert.in/v1"
+    : "https://appwrite.engexpert.in/v1";
 const EMAIL_FUNCTION_ID = "63d9f6fb5afb9ab72de6";
 const TEST_CSV_LINK =
   "https://engapp-csvfile.s3.ap-south-1.amazonaws.com/user-format.csv";
